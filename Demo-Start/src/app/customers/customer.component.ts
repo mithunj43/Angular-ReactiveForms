43359e6654cc6bec1042dedb8f012c23a0618f6e
mithun <mithunj43@gmail.com>
2023-07-25 19:51:15 +0530

    SetValue and PatchValue

--- Demo-Start/src/app/customers/customer.component.ts
+++ Demo-Start/src/app/customers/customer.component.ts
@@ -9,14 +9,14 @@ import { Customer } from './customer';
   styleUrls: ['./customer.component.css']
 })
 export class CustomerComponent implements OnInit {
-  customerForm:FormGroup;
+  customerForm: FormGroup;
   customer = new Customer();
 
   constructor() { }
 
   ngOnInit(): void {
     this.customerForm = new FormGroup({
-      firstName:new FormControl(),
+      firstName: new FormControl(),
       lastName: new FormControl(),
       email: new FormControl(),
       sendCatalog: new FormControl(true)
@@ -26,5 +26,22 @@ export class CustomerComponent implements OnInit {
   save(): void {
     console.log(this.customerForm);
     console.log('Saved: ' + JSON.stringify(this.customerForm.value));
+  }
+
+  // populateTestData(): void {
+  //   this.customerForm.setValue({
+  //     firstName: 'Pooja',
+  //     lastName: 'Mithun',
+  //     email: 'pooja@gmail.com',
+  //     sendCatalog: false
+  //   })
+  // }
+
+  populateTestData(): void {
+    this.customerForm.patchValue({
+      firstName: 'Pooja',
+      lastName: 'Mithun',
+      sendCatalog: false
+    })
   }
 }
