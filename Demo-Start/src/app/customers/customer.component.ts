784cf0ebdb9a1d1161976c12602342cec0e95465
mithun <mithunj43@gmail.com>
2023-07-27 13:11:44 +0530

    Custom Validation with parameters

--- Demo-Start/src/app/customers/customer.component.ts
+++ Demo-Start/src/app/customers/customer.component.ts
@@ -1,13 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators, AbstractControl } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, AbstractControl, ValidatorFn } from '@angular/forms';
 
 import { Customer } from './customer';
 
-function ratingRange(c: AbstractControl): { [key: string]: boolean } | null {
-  if (c.value != null && (isNaN(c.value) || c.value < 1 || c.value > 5)) {
-    return { 'range': true }
+function ratingRange(min: number, max: number): ValidatorFn {
+  return (c: AbstractControl): { [key: string]: boolean } | null => {
+    if (c.value != null && (isNaN(c.value) || c.value < min || c.value > max)) {
+      return { 'range': true }
+    }
+    return null;
   }
-  return null;
 }
 
 @Component({
@@ -30,7 +32,7 @@ export class CustomerComponent implements OnInit {
       phone: '',
       notification: 'email',
       sendCatalog: true,
-      rating: [null, ratingRange]
+      rating: [null, ratingRange(1,5)]
     });
   }
 
